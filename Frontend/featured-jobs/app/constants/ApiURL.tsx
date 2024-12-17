b6aa477c54f1aa18e7c960eb763d98ccc3c80b13
Nishant <ntih5565@gmail.com>
2024-12-18 02:02:34 +0530

    App api changed to azure

--- Frontend/featured-jobs/app/constants/ApiURL.tsx
+++ Frontend/featured-jobs/app/constants/ApiURL.tsx
@@ -1,8 +1,8 @@
 // ! Production URL on Azure
 
-// export const API_URL = 'https://fgtbackend-f6emf0f3dkhdegfb.canadacentral-01.azurewebsites.net/';
+export const API_URL = 'https://fgtbackend-f6emf0f3dkhdegfb.canadacentral-01.azurewebsites.net/';
 
 
-export const API_URL = 'https://fjt-bakckend.onrender.com';
+// export const API_URL = 'https://fjt-bakckend.onrender.com';
 
 
